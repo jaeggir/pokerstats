9f170441c569f55d1b22e3eb0354ee18ca3a39b1
Roger Jäggi <jaeggir@gmail.com>
2014-03-21 12:22:18 +0100

    Integrate Players.

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -4,9 +4,23 @@ angular.module('pokerstatsApp.controllers', []);
 angular.module('pokerstatsApp.directives', []);
 angular.module('pokerstatsApp.services', []);
 
-var pokerstatsApp = angular.module('pokerstatsApp', ['ngResource', 'ngRoute', 'ngCookies']);
+var pokerstatsApp = angular.module('pokerstatsApp', [
+    'ngResource',
+    'ngRoute',
+    'ngCookies',
+
+    'pokerstatsApp.controllers',
+    'pokerstatsApp.directives',
+    'pokerstatsApp.services'
+
+]);
 
 pokerstatsApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'views/main.html', controller: 'MainController'});
-    $routeProvider.otherwise({redirectTo: '/'});
+    $routeProvider.when('/players', {templateUrl: 'views/players.html', controller: 'PlayersController'});
+    $routeProvider.otherwise({redirectTo: '/players'});
 }]);
+
+pokerstatsApp.factory('playersResource', function($resource) {
+    return $resource('rest/1.0/player/');
+});
--- src/main/webapp/scripts/controllers.js
+++ src/main/webapp/scripts/controllers.js
@@ -5,3 +5,12 @@ var controllers = angular.module('pokerstatsApp.controllers');
 controllers.controller('MainController', function MainController($scope) {
 
 });
+
+controllers.controller('PlayersController', function PlayersController($scope, playersResource) {
+
+    $scope.players = {};
+    playersResource.query().$promise.then(function(players) {
+        $scope.players = players;
+    });
+
+});
