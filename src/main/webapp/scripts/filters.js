abd79586f5c00aa35de273afed76070867ce6882
Roger Jäggi <jaeggir@gmail.com>
2014-03-22 14:00:28 +0100

    remove unused code.

--- src/main/webapp/scripts/filters.js
+++ src/main/webapp/scripts/filters.js
@@ -13,9 +13,3 @@ filters.filter('defaultDateTime', function defaultDateTime() {
         return moment(dateString).format('Do MMMM YYYY, h:mm');
     };
 });
-
-filters.filter('filterBySeason', function filterBySeason() {
-    return function (season) {
-        return season;
-    };
-});
