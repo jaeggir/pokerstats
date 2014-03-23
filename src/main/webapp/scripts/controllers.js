9f7fbbdb96e6120c9614ae1cd42a67f9897f445c
Roger Jäggi <jaeggir@gmail.com>
2014-03-23 16:51:59 +0100

    Remove unused controller.

--- src/main/webapp/scripts/controllers.js
+++ src/main/webapp/scripts/controllers.js
@@ -2,19 +2,6 @@
 
 var controllers = angular.module('pokerstatsApp.controllers');
 
-controllers.controller('MainController', function MainController($scope, Seasons, Event) {
-    $scope.season = {};
-    $scope.events = [];
-    Seasons.query({filter: 'current'}).$promise.then(function (season) {
-        $scope.season = season[0];
-        angular.forEach($scope.season.events, function (eventUuid) {
-            Event.get({uuid: eventUuid}).$promise.then(function (event) {
-                $scope.events.push(event);
-            });
-        });
-    });
-});
-
 controllers.controller('PlayersController', function PlayersController($scope, $modal, $filter, Players) {
 
     $scope.players = {};
