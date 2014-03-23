45d5d4d8570431361316cb97715723995d1218ec
Roger Jäggi <jaeggir@gmail.com>
2014-03-23 23:57:14 +0100

    First steps to implement game tracking..

--- src/main/webapp/scripts/controllers/TournamentCtrl.js
+++ src/main/webapp/scripts/controllers/TournamentCtrl.js
@@ -7,9 +7,17 @@ controllers.controller('TournamentController', function TournamentController($sc
 
     $scope.tournament = {};
     $scope.results = [];
+    $scope.anteTotal = 0;
     Tournament.get({uuid: $routeParams.tournamentUuid}).$promise.then(function (tournament) {
         $scope.tournament = tournament;
-        $scope.results = TournamentResults.query({uuid: tournament.uuid});
+        TournamentResults.query({uuid: tournament.uuid}).$promise.then(function (results) {
+            $scope.results = results;
+            angular.forEach($scope.results, function (result) { $scope.anteTotal += result.ante; });
+        });
     });
+
+    $scope.killPlayer = function (playerUuid) {
+        console.log('TODO open dialog to "kill" player with id=' + playerUuid);
+    };
 
 });
