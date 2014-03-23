8344b4020de6ea413d09ad9f3546faf0d0bedf7d
Roger Jäggi <jaeggir@gmail.com>
2014-03-23 19:41:41 +0100

    add logic to add tournaments.

--- src/main/webapp/scripts/controllers.js
+++ src/main/webapp/scripts/controllers.js
@@ -120,6 +120,15 @@ controllers.controller('EventController', function EventController($scope, $rout
         $scope.tournaments = Tournaments.query({eventUuid: $scope.event.uuid});
     });
 
+    $scope.isAvailable = function () {
+        // returns true if the event date is today or later
+        return $scope.event !== null && $scope.event.date > (new Date().getTime() - 24 * 60 * 60 * 1000);
+    };
+
+    $scope.addTournament = function () {
+        console.log('TODO implement add Tournament.');
+    };
+
 });
 
 controllers.controller('VenuesController', function VenuesController($scope, Venues, angulargmContainer) {
